951fcecd3c9e5a1739d71f4f955f411c1da7a9c5
Irving Popovetsky <irving@honeycomb.io>
2022-10-16 21:49:24 -0700

    ugh, tabs
    Signed-off-by: Irving Popovetsky <irving@honeycomb.io>

--- crude.go
+++ crude.go
@@ -28,60 +28,60 @@ const DefaultRulesContent = `# For docs on Refinery Rules, see: https://docs.hon
 Sampler = "DeterministicSampler"
 SampleRate = 1
 `
+
 var tracer = otel.Tracer("crude")
 
 func main() {
-    // use honeycomb distro to setup OpenTelemetry SDK
-    otelShutdown, err := launcher.ConfigureOpenTelemetry()
-    if err != nil {
-        log.Fatalf("error setting up OTel SDK - %e", err)
-    }
-    defer otelShutdown()
-
+	// use honeycomb distro to setup OpenTelemetry SDK
+	otelShutdown, err := launcher.ConfigureOpenTelemetry()
+	if err != nil {
+		log.Fatalf("error setting up OTel SDK - %e", err)
+	}
+	defer otelShutdown()
 
 	router := gin.Default()
-  router.Use(otelgin.Middleware("crude"))
-  router.Static("/assets", "./assets")
+	router.Use(otelgin.Middleware("crude"))
+	router.Static("/assets", "./assets")
 	router.LoadHTMLGlob("templates/*")
 	router.Use(sessions.Sessions("crudesession", cookie.NewStore([]byte("BoxContainsLiveB33s!"))))
 
 	router.GET("/", homePage)
 	router.POST("/", rulesReceiver)
 
-  router.Use(gin.CustomRecovery(func(c *gin.Context, recovered interface{}) {
-    if err, ok := recovered.(string); ok {
-      c.HTML(http.StatusInternalServerError, "error.html.tmpl", gin.H{
-        "Code": http.StatusInternalServerError,
-        "Message": err,
-      })
-    }
-    c.AbortWithStatus(http.StatusInternalServerError)
-  }))
+	router.Use(gin.CustomRecovery(func(c *gin.Context, recovered interface{}) {
+		if err, ok := recovered.(string); ok {
+			c.HTML(http.StatusInternalServerError, "error.html.tmpl", gin.H{
+				"Code":    http.StatusInternalServerError,
+				"Message": err,
+			})
+		}
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}))
 
-  // if environment variable ENV=development, then run in debug mode
-  if os.Getenv("ENV") == "development" {
-	  router.Run("localhost:5000")
-  } else {
-    router.Run(":8000")
-  }
+	// if environment variable ENV=development, then run in debug mode
+	if os.Getenv("ENV") == "development" {
+		router.Run("localhost:5000")
+	} else {
+		router.Run(":8000")
+	}
 }
 
 func homePage(c *gin.Context) {
 	session := sessions.Default(c)
 
-  rulesContent, err := getRulesFromS3(c)
-  if err != nil {
-    rulesContent, err = getRulesFromTmpFile(c)
-    if err != nil {
-      rulesContent = DefaultRulesContent
-    }
-  }
+	rulesContent, err := getRulesFromS3(c)
+	if err != nil {
+		rulesContent, err = getRulesFromTmpFile(c)
+		if err != nil {
+			rulesContent = DefaultRulesContent
+		}
+	}
 
-  // clear the flashes
-  // https://github.com/gin-gonic/contrib/issues/54
-  success_flashes := session.Flashes("Success")
-  error_flashes := session.Flashes("Error")
-  session.Save()
+	// clear the flashes
+	// https://github.com/gin-gonic/contrib/issues/54
+	success_flashes := session.Flashes("Success")
+	error_flashes := session.Flashes("Error")
+	session.Save()
 
 	otelgin.HTML(c, http.StatusOK, "index.html.tmpl", gin.H{
 		"rules_content": rulesContent,
@@ -94,50 +94,49 @@ func getRulesFromTmpFile(c *gin.Context) (string, error) {
 	_, span := tracer.Start(c.Request.Context(), "getRulesFromTmpFile", oteltrace.WithAttributes())
 	defer span.End()
 
-  tmpFile := "/tmp/rules.txt"
-  rulesContent, err := os.ReadFile(tmpFile)
-  if err != nil {
-    span.SetAttributes(attribute.String("error", err.Error()))
-    return "", err
-  }
-  span.SetAttributes(attribute.String("rulesContent", string(rulesContent)))
-  return string(rulesContent), nil
+	tmpFile := "/tmp/rules.txt"
+	rulesContent, err := os.ReadFile(tmpFile)
+	if err != nil {
+		span.SetAttributes(attribute.String("error", err.Error()))
+		return "", err
+	}
+	span.SetAttributes(attribute.String("rulesContent", string(rulesContent)))
+	return string(rulesContent), nil
 }
 
 func getRulesFromS3(c *gin.Context) (string, error) {
-  _, span := tracer.Start(c.Request.Context(), "getRulesFromS3", oteltrace.WithAttributes())
-  defer span.End()
+	_, span := tracer.Start(c.Request.Context(), "getRulesFromS3", oteltrace.WithAttributes())
+	defer span.End()
 
-  cfg, err := awsconfig.LoadDefaultConfig(c)
-  if err != nil {
-    span.SetAttributes(attribute.String("error", err.Error()))
-    span.SetAttributes(attribute.String("error_stage", "LoadDefaultConfig"))
-    return "", err
-  }
+	cfg, err := awsconfig.LoadDefaultConfig(c)
+	if err != nil {
+		span.SetAttributes(attribute.String("error", err.Error()))
+		span.SetAttributes(attribute.String("error_stage", "LoadDefaultConfig"))
+		return "", err
+	}
 
-  client := s3.NewFromConfig(cfg)
-  result, err := client.GetObject(c, &s3.GetObjectInput{
-    Bucket: aws.String("irving-fafo"),
-    Key:    aws.String("rules.txt"),
-  })
+	client := s3.NewFromConfig(cfg)
+	result, err := client.GetObject(c, &s3.GetObjectInput{
+		Bucket: aws.String("irving-fafo"),
+		Key:    aws.String("rules.txt"),
+	})
 
-  if err != nil {
-    span.SetAttributes(attribute.String("error", err.Error()))
-    span.SetAttributes(attribute.String("error_stage", "GetObject"))
-    return "", err
-  }
+	if err != nil {
+		span.SetAttributes(attribute.String("error", err.Error()))
+		span.SetAttributes(attribute.String("error_stage", "GetObject"))
+		return "", err
+	}
 
-  rulesContent, err := ioutil.ReadAll(result.Body)
-  if err != nil {
-    span.SetAttributes(attribute.String("error", err.Error()))
-    span.SetAttributes(attribute.String("error_stage", "ioutil.ReadAll"))
-    return "", err
-  }
+	rulesContent, err := ioutil.ReadAll(result.Body)
+	if err != nil {
+		span.SetAttributes(attribute.String("error", err.Error()))
+		span.SetAttributes(attribute.String("error_stage", "ioutil.ReadAll"))
+		return "", err
+	}
 
-  span.SetAttributes(attribute.String("rulesContent", string(rulesContent)))
-  return string(rulesContent), nil
+	span.SetAttributes(attribute.String("rulesContent", string(rulesContent)))
+	return string(rulesContent), nil
 }
-
 
 func rulesReceiver(c *gin.Context) {
 	rulesContent := c.PostForm("rules_content")
@@ -147,62 +146,62 @@ func rulesReceiver(c *gin.Context) {
 
 	session := sessions.Default(c)
 
-  err := writeRulesToTmpFile(c, rulesContent)
-  if err != nil {
-    span.SetAttributes(attribute.String("error", err.Error()))
-	  session.AddFlash( fmt.Sprintf("Something went wrong: %s", err), "Error")
-    session.Save()
-    c.Redirect(http.StatusFound, "/")
-  }
+	err := writeRulesToTmpFile(c, rulesContent)
+	if err != nil {
+		span.SetAttributes(attribute.String("error", err.Error()))
+		session.AddFlash(fmt.Sprintf("Something went wrong: %s", err), "Error")
+		session.Save()
+		c.Redirect(http.StatusFound, "/")
+	}
 
-  err2 := writeRulesToS3(c, rulesContent)
-  if err2 != nil {
-    span.SetAttributes(attribute.String("error", err2.Error()))
-	  session.AddFlash( fmt.Sprintf("Something went wrong: %s", err2), "Error")
-    session.Save()
-    c.Redirect(http.StatusFound, "/")
-  }
+	err2 := writeRulesToS3(c, rulesContent)
+	if err2 != nil {
+		span.SetAttributes(attribute.String("error", err2.Error()))
+		session.AddFlash(fmt.Sprintf("Something went wrong: %s", err2), "Error")
+		session.Save()
+		c.Redirect(http.StatusFound, "/")
+	}
 
 	session.AddFlash("Rules saved successfully", "Success")
-  session.Save()
-  c.Redirect(http.StatusFound, "/")
+	session.Save()
+	c.Redirect(http.StatusFound, "/")
 }
 
 func writeRulesToTmpFile(c *gin.Context, rulesContent string) error {
 	_, span := tracer.Start(c.Request.Context(), "writeRulesToTmpFile", oteltrace.WithAttributes())
 	defer span.End()
 
-  tmpFile := "/tmp/rules.txt"
-  err := os.WriteFile(tmpFile, []byte(rulesContent), 0644)
-  if err != nil {
-    span.SetAttributes(attribute.String("error", err.Error()))
-    return err
-  }
-  return nil
+	tmpFile := "/tmp/rules.txt"
+	err := os.WriteFile(tmpFile, []byte(rulesContent), 0644)
+	if err != nil {
+		span.SetAttributes(attribute.String("error", err.Error()))
+		return err
+	}
+	return nil
 }
 
 // write rules file to S3
 func writeRulesToS3(c *gin.Context, rulesContent string) error {
-  _, span := tracer.Start(c.Request.Context(), "writeRulesToS3", oteltrace.WithAttributes())
-  defer span.End()
+	_, span := tracer.Start(c.Request.Context(), "writeRulesToS3", oteltrace.WithAttributes())
+	defer span.End()
 
-  cfg, err := awsconfig.LoadDefaultConfig(c)
+	cfg, err := awsconfig.LoadDefaultConfig(c)
 	if err != nil {
-    span.SetAttributes(attribute.String("error", err.Error()))
-    return err
+		span.SetAttributes(attribute.String("error", err.Error()))
+		return err
 	}
 
 	client := s3.NewFromConfig(cfg)
-  _, err = client.PutObject(c, &s3.PutObjectInput{
-    Bucket: aws.String("irving-fafo"),
-    Key:    aws.String("rules.txt"),
-    Body:   strings.NewReader(rulesContent),
-  })
+	_, err = client.PutObject(c, &s3.PutObjectInput{
+		Bucket: aws.String("irving-fafo"),
+		Key:    aws.String("rules.txt"),
+		Body:   strings.NewReader(rulesContent),
+	})
 
-  if err != nil {
-    span.SetAttributes(attribute.String("error", err.Error()))
-    return err
-  }
+	if err != nil {
+		span.SetAttributes(attribute.String("error", err.Error()))
+		return err
+	}
 
-  return nil
+	return nil
 }
